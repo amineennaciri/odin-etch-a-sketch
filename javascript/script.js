b000eb82d9d8deb4fec664c189aba2bf48635d55
amineennaciri <spitlight.enn@gmail.com>
2022-11-11 12:20:09 +0100

    the project is complete let's merge to main

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,44 @@
+
 // setting up the button logic
 const btn = document.querySelector('button')
 let result = btn.addEventListener('click',gridNum)
+
 function gridNum(){
     let boxNum = prompt('Enter the number of squares per side for the new grid.')
+    if(boxNum>100){
+        gridNum()
+    }
+    else{
+
+    
+    let gridBox = boxNum**2
+    console.log(gridBox)
     console.log(boxNum)
-}
+    // creating the 16x16 boxes with javascript
+    const container = document.getElementsByClassName('container');
+    for(let i=1; i<= gridBox;i++){
+        const div = document.createElement('div');
+        div.classList.add("item");
+        /* console.log(div); */
+        /* console.log(container[0]); */
+        container[0].appendChild(div);
+    }
 
+    // adding css to the boxes
+    const box = document.querySelectorAll('.item')
+    /* console.log(box[0]) */
+    for(let i = 0;i<=box.length-1;i++){
+        box[i].style.width=`${100/boxNum}%`// because of 100% divided by 16 boxes
+        box[i].style.height=`${100/boxNum}%`
+    }
+    //setting up the hover
+    for(let i = 0;i<=box.length-1;i++){
+        box[i].addEventListener('mouseenter',hovering)
+        box[i].addEventListener('mouseleave',unHovering)
+    }
 
-// creating the 16x16 boxes with javascript
-const container = document.getElementsByClassName('container');
-for(let i=1; i<= 256;i++){
-    const div = document.createElement('div');
-    div.classList.add("item");
-    /* console.log(div); */
-    /* console.log(container[0]); */
-    container[0].appendChild(div);
-}
-
-// adding css to the boxes
-const box = document.querySelectorAll('.item')
-/* console.log(box[0]) */
-for(let i = 0;i<=box.length-1;i++){
-    box[i].style.width=`${100/16}%`// because of 100% divided by 16 boxes
-    box[i].style.height=`${100/16}%`
-}
-
-//setting up the hover
-for(let i = 0;i<=box.length-1;i++){
-    box[i].addEventListener('mouseenter',hovering)
-    box[i].addEventListener('mouseleave',unHovering)
+    return gridBox
+    }
 }
 
 function hovering(e){
